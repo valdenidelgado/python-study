2a08f35a179a2ffc835aa86f5f8d45bfcc60409c
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-06-02 18:52:21 -0300

    two class- comment and method print

---
+++ day01/comments.py
@@ -0,0 +1,21 @@
+"""
+ aqui são uns do modos de comentar o codigo python, esse geralmente são para bloco de texto ou documentação, pode ser usado com 3 aspas duplas ou simples '''tipo assim '''
+"""
+
+# esse é um comentario in line, para comentar uma linha ou um metodo especifico
+
+
+'''
+    Perguntas
+    01 - Qual a extensão do arquivo Python?
+    R> A extensão do arquivo Python é .py
+
+    02 - Qual o resultado do codigo abaixo
+    """
+    print('Hello world!')
+    """
+    # print("Olá mundo!")
+    print('FIM')
+
+    R> FIM
+'''
---
+++ day01/print.py
@@ -0,0 +1,20 @@
+"""
+    criar um separador usando o comando print : 824.176.070-18
+    sep="" = sep é um separado de string
+    end="" = é para manter a string na mesma linha, se tiver vazio, a string ira vir colado com o primeiro print
+"""
+# se o print tiver com virgulas a cada string e sem o sep, ele ira dar um espaço entre elas por default
+print('824', '176', '070', sep='.', end='-')
+print('18')
+
+
+'''
+    Pergunta
+    01 - Qual a saida do codigo abaixo?
+    """
+        print('Estou', 'aprendendo', 'Python', sep='-', end=' ')
+        print('isso é muito', 'legal', sep='-', end='')
+    """
+    
+    R> print('isso é muito', 'legal', sep='-', end='')
+'''
