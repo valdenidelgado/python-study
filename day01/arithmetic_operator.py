17800550a92fcc68f96c0f61503f20cf00a9b754
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-06-02 20:39:03 -0300

    course about arithmetic operator

---
+++ day01/arithmetic_operator.py
@@ -0,0 +1,19 @@
+"""
+    +, - = adição e subtração
+    * = multiplicação
+    / = divisão
+    // = a divisão é arredondado para o numero inteiro, tipo, 10.7, vira 11
+    ** = exponenciação, um numero elevado ao outro, 2**2, 2 elevado ao 2
+    % = resto da divisão
+    () = São usados para alterar a precedencia das contas
+"""
+
+print('Adição "+" ', 10 + 10)
+print('Subtração "-" ', 10 - 10)
+print('Multiplicação "*" ', 10 * 10)
+print('Multiplicação de string ex: 5 * "a"', 5 * 'a')
+print('Divisão "/" ', 10 / 10)
+print('Divisão arredondado "//" ', 10 // 10)
+print('Valor elevado, exponenciação "**" ', 10 ** 10)
+print('Resto da divisão "%" ', 10 % 10)
+print('Precedencia,"()"', 10 * (10+10))
