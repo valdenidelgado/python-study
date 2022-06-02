c537361ac2432fd6a01964c407fdd2c993c712f0
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-06-02 19:05:38 -0300

    course class about string

---
+++ day01/string.py
@@ -0,0 +1,12 @@
+"""
+    no python str = string
+"""
+
+
+print('string com aspas simples')
+print("string com aspas duplas")
+print('para adicionar aspas dentro da string, tem que trocar entre elas "tipo" assim, começa com uma e dentro, usa outra')
+print("existe o caractere de escape, que ignora as aspas, \"tipo\" contra barra")
+print("com o \n ele vai quebrar a linha")
+print(r"com o r no começo, não \n executa nada dentro do bloco print tipo o \n")
+
